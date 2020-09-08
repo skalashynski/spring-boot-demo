8d23c2492c0f41e8eab8e8001906d4d9fc7ac1e5
siarhei kalashynski <skalashynski@productiveedge.com>
2020-09-08 20:28:30 +0300

    mysql resources/jpa tests/mysql driver

--- src/main/java/com/example/demo/repository/StudentDAO.java
+++ src/main/java/com/example/demo/repository/StudentDAO.java
@@ -1,12 +1,17 @@
 package com.example.demo.repository;
 
 import com.example.demo.bean.Student;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface StudentDAO extends CrudRepository<Student, Long> {
     List<Student> findByFirstName(String name);
+
+    @Query(value = "SELECT * FROM STUDENT s WHERE s.birthday BETWEEN :from AND :to", nativeQuery = true)
+    List<Student> findBetweenBirthdays(Date from, Date to);
 }
---
+++ src/test/java/com/example/demo/repository/StudentDAOTest.java
@@ -0,0 +1,51 @@
+package com.example.demo.repository;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTypeExcludeFilter;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+
+import java.sql.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@ExtendWith(SpringExtension.class)
+class StudentDAOTest {
+    /*
+
+    @Autowired
+    private DataSource dataSource;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    @Autowired
+    private EntityManager entityManager;
+
+    */
+    @Autowired
+    private StudentDAO studentDAO;
+
+    @Test
+    void injectedComponentsAreNotNUll() {
+        /*
+
+        assertNotNull(dataSource);
+        assertNotNull(jdbcTemplate);
+        assertNotNull(entityManager);
+
+        */
+        assertNotNull(studentDAO);
+    }
+
+    @Test
+    void betweenDates() {
+        assertEquals(3, studentDAO.findBetweenBirthdays(Date.valueOf("1900-03-17"), Date.valueOf("2000-03-17")).size());
+    }
+}
