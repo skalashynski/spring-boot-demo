b00276e4e762e6af8b072fdc48a7248a921d373e
siarhei kalashynski <skalashynski@productiveedge.com>
2020-09-08 12:23:50 +0300

    adding swagger configuration

---
+++ README.md
@@ -0,0 +1,3 @@
+to open swagger use URL: http://localhost:8081/swagger-ui.html
+
+to open swagger api use URL: http://localhost:8081/v2/api-docs
--- src/main/java/com/example/demo/DemoSpringApplication.java
+++ src/main/java/com/example/demo/DemoSpringApplication.java
@@ -3,15 +3,15 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @ComponentScan("com.example.demo")
 //@EnableAutoConfiguration
 public class DemoSpringApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSpringApplication.class, args);
 	}
-
 }
