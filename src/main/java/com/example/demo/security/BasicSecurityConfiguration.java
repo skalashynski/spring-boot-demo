bb06b9210141d65aefb981985182a79cba27c96a
siarhei kalashynski <skalashynski@productiveedge.com>
2020-09-08 20:28:59 +0300

    spring configuration

---
+++ src/main/java/com/example/demo/security/BasicSecurityConfiguration.java
@@ -0,0 +1,39 @@
+package com.example.demo.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+@Configuration
+public class BasicSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        auth
+            .inMemoryAuthentication()
+                .withUser("user")
+                    .password(passwordEncoder.encode("password"))
+                    .roles("USER")
+                .and()
+                .withUser("admin")
+                    .password(passwordEncoder.encode("admin"))
+                    .roles("USER", "ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .csrf().disable(); //disabling csrf allows execute POST/PUT/DELETE HTTP methods on REST
+    }
+}
