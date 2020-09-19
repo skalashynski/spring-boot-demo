213af32420f53a0e0b81bb288f1be47e6abad56e
siarhei kalashynski <skalashynski@productiveedge.com>
2020-09-19 13:49:30 +0300

    stable sql script

---
+++ sql-scripts/setup.sql
@@ -0,0 +1,40 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+-- -----------------------------------------------------
+-- DATABASE  mydb
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- DATABASE  mydb
+-- -----------------------------------------------------
+drop database if exists `mydb`;
+create DATABASE IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+USE `mydb` ;
+
+-- -----------------------------------------------------
+-- Table `mydb`.`user`
+-- -----------------------------------------------------
+create TABLE IF NOT EXISTS `mydb`.`student` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `first_name` VARCHAR(45) NOT NULL,
+  `last_name` VARCHAR(45) NOT NULL,
+  `birthday` DATE DEFAULT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 31
+DEFAULT CHARACTER SET = utf8 COLLATE utf8_unicode_ci;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+INSERT INTO student (id, first_name, last_name, birthday) VALUES
+(1, 'Aliko', 'Dangote', '1997-03-17'),
+(2, 'Bill', 'Gates', '1955-12-31'),
+(3, 'Folrunsho', 'Alakija', '1997-12-31');
+
