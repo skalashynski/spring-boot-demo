e06569049563af89039d19333ca64b32a2b9543a
skalashynski <sergei.kalashynski@gmail.com>
2021-06-06 13:53:16 +0300

    basic auth. In-memory users implementing

--- src/main/java/com/skalashynski/spring/springboot/config/AppConfig.java
+++ src/main/java/com/skalashynski/spring/springboot/config/AppConfig.java
@@ -1,12 +1,15 @@
 package com.skalashynski.spring.springboot.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class AppConfig {
-    /*@Bean
+    @Bean
     public static PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }*/
+        return new BCryptPasswordEncoder(10);
+    }
 
 }
--- src/main/java/com/skalashynski/spring/springboot/config/BasicSecurityConfiguration.java
+++ src/main/java/com/skalashynski/spring/springboot/config/BasicSecurityConfiguration.java
@@ -1,15 +1,23 @@
 package com.skalashynski.spring.springboot.config;
 
 import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 @AllArgsConstructor
 public class BasicSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    private final PasswordEncoder passwordEncoder;
+
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http
@@ -22,5 +30,17 @@ public class BasicSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    //how we retrieve users from DB
+    @Bean
+    @Override
+    protected UserDetailsService userDetailsService() {
+        UserDetails annaSmith = User.builder()
+                .username("annasmith")
+                .password(passwordEncoder.encode("password"))
+                .roles("STUDENT")//ROLE_STUDENT
+                .build();
+        return new InMemoryUserDetailsManager(annaSmith);
     }
 }
