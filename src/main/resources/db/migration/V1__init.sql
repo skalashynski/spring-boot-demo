94a23ff544693dd4399e2bef8f6ae82f6ab292f1
Dima Nevar <nevar.dima@gmail.com>
2021-09-20 17:32:58 +0300

    ***.added flyway for database

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,31 @@
+CREATE TABLE IF NOT EXISTS STUDENTS
+(
+    id         SERIAL PRIMARY KEY,
+    first_name VARCHAR(250) NOT NULL,
+    last_name  VARCHAR(250) NOT NULL,
+    birthday   DATE      DEFAULT NULL,
+    created_at TIMESTAMP DEFAULT NULL
+);
+
+CREATE TABLE IF NOT EXISTS APP_USERS
+(
+    id            SERIAL PRIMARY KEY,
+    first_name    VARCHAR(250) NOT NULL,
+    last_name     VARCHAR(250) NOT NULL,
+    email         VARCHAR(250) NOT NULL,
+    username      VARCHAR(250) NOT NULL,
+    password      VARCHAR(250) NOT NULL,
+    is_locked     BOOLEAN NULL,
+    enabled       BOOLEAN NULL,
+    app_user_role VARCHAR(10) DEFAULT 'STUDENT'
+);
+
+CREATE TABLE IF NOT EXISTS TOKENS_CONFIRMATION
+(
+    id           SERIAL PRIMARY KEY,
+    token        VARCHAR(250) NOT NULL,
+    created_at   TIMESTAMP    NOT NULL,
+    expires_at   TIMESTAMP    NOT NULL,
+    confirmed_at TIMESTAMP NULL,
+    app_user_id  INT          NOT NULL
+);
