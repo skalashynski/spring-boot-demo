161bbc9220d02996506eb4e995f28884ce715c11
Viktor Shilay <viktor.shilay@gmail.com>
2022-08-25 22:49:36 +0300

    add tests using spock framework

--- src/test/groovy/com/skalashynski/spring/springboot/repository/ConfirmationTokenRepositoryTest.groovy
+++ src/test/groovy/com/skalashynski/spring/springboot/repository/ConfirmationTokenRepositoryTest.groovy
@@ -1,14 +1,9 @@
 package com.skalashynski.spring.springboot.repository
 
 import com.skalashynski.spring.springboot.DatabaseSpecification
-import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.ContextConfiguration
-import org.springframework.test.context.junit4.SpringRunner
 
-@RunWith(SpringRunner.class)
-@ContextConfiguration
 @SpringBootTest
 class ConfirmationTokenRepositoryTest extends DatabaseSpecification {
     @Autowired
@@ -24,15 +19,13 @@ class ConfirmationTokenRepositoryTest extends DatabaseSpecification {
         sql.execute(new File('src/test/resources/cleanup.sql').text)
     }
 
-
-    def void "find by token '#token'"(String token) {
-
-        when: "Calling DB"
-            boolean present = confirmationTokenRepository.findByToken(token).isPresent();
+    def "find by token '#token'"(String token) {
+        when:
+        boolean present = confirmationTokenRepository.findByToken(token).isPresent();
         then:
-            present
+        present
         where:
-            token                    | _
-            "AlikoDangotegangote123" | _
+        token                    | _
+        "AlikoDangotegangote123" | _
     }
 }
--- src/test/groovy/com/skalashynski/spring/springboot/web/controller/StudentControllerTest.groovy
+++ src/test/groovy/com/skalashynski/spring/springboot/web/controller/StudentControllerTest.groovy
@@ -3,7 +3,6 @@ package com.skalashynski.spring.springboot.web.controller
 import com.skalashynski.spring.springboot.Application
 import com.skalashynski.spring.springboot.DatabaseSpecification
 import com.skalashynski.spring.springboot.entity.Student
-import com.skalashynski.spring.springboot.util.ResourceUtils
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
@@ -12,9 +11,7 @@ import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
 import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.context.ContextConfiguration
-import org.springframework.web.client.HttpClientErrorException
-import spock.lang.Unroll
+import org.springframework.web.client.RestClientException
 
 import java.time.LocalDate
 import java.time.LocalDateTime
@@ -23,8 +20,7 @@ import java.time.LocalDateTime
         classes = Application.class,
         webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT
 )
-@ActiveProfiles(value = "test")
-@ContextConfiguration
+@ActiveProfiles("test")
 class StudentControllerTest extends DatabaseSpecification {
 
     private static final String STUDENT_URL = "http://localhost:8080/demo/api/v1/student/"
@@ -35,153 +31,202 @@ class StudentControllerTest extends DatabaseSpecification {
     @Value('${test.token}')
     private String TOKEN_1_YEAR_DURATION;
 
-    static String setupContent
-    static String cleanupContent
-
-    def setupSpec() {
-        cleanupContent = ResourceUtils.getResourceContents('cleanup.sql')
-        setupContent = ResourceUtils.getResourceContents('data.sql')
-    }
-
-    @SuppressWarnings("unused")
     def setup() {
         headers.add("Authorization", TOKEN_1_YEAR_DURATION)
-        sql.execute(setupContent)
+        sql.execute(new File('src/test/resources/data.sql').text)
     }
 
-    @SuppressWarnings("unused")
     def cleanup() {
-        sql.execute(cleanupContent)
+        sql.execute(new File('src/test/resources/cleanup.sql').text)
     }
 
-    @Unroll
-    def void "Student get by #id, expected #expectedHttpCode"(Long id) {
+    def "Student get by #id, expected #expectedHttpCode"(Long id) {
         when:
-            def response
-            def actualErrorMessage
-            try {
-                response = restTemplate.exchange(
-                        STUDENT_URL + id
-                        , HttpMethod.GET
-                        , new HttpEntity<>(headers)
-                        , Student.class)
-            } catch (Exception e) {
-                actualErrorMessage = e.getMessage()
-            }
+        def response
+        def actualErrorMessage
+        try {
+            response = restTemplate.exchange(
+                    STUDENT_URL + id
+                    , HttpMethod.GET
+                    , new HttpEntity<>(headers)
+                    , Student)
+        } catch (Exception e) {
+            actualErrorMessage = e.getMessage()
+        }
         then:
-            def actualStudent = response.getBody()
-            if (actualStudent != null) {
-                assert actualStudent.getId() == result.getId()
-                assert actualStudent.getFirstName() == result.getFirstName()
-                assert actualStudent.getLastName() == result.getLastName()
-            } else {
-                assert actualStudent == result
-            }
-            assert response.getStatusCode() == expectedHttpCode
+        def actualStudent = response.getBody()
+        actualStudent == result
+        response.getStatusCode() == expectedHttpCode
         where:
-            id || expectedHttpCode     || result
-            1  || HttpStatus.OK        || new Student(1, 'Aliko', 'Dangote', LocalDate.parse('1997-03-17'), LocalDateTime.now())
-            2  || HttpStatus.OK        || new Student(2, 'Bill', 'Gates', LocalDate.parse('1955-12-31'), LocalDateTime.now())
-            3  || HttpStatus.OK        || new Student(3, 'Folrunsho', 'Alakija', LocalDate.parse('1997-12-31'), LocalDateTime.now())
-            7  || HttpStatus.NOT_FOUND || null
+        id | expectedHttpCode     || result
+        1  | HttpStatus.OK        || new Student(1, 'Aliko', 'Dangote', LocalDate.of(1997, 3, 17), LocalDateTime.of(2021, 12, 12, 1, 2, 3))
+        2  | HttpStatus.OK        || new Student(2, 'Bill', 'Gates', LocalDate.of(1955, 12, 31), LocalDateTime.of(2021, 12, 12, 1, 2, 3))
+        3  | HttpStatus.OK        || new Student(3, 'Folrunsho', 'Alakija', LocalDate.of(1997, 12, 31), LocalDateTime.of(2021, 12, 12, 1, 2, 3))
+        7  | HttpStatus.NOT_FOUND || null
     }
 
-    @Unroll
-    def void "Students get all, expected 6"() {
+    def "Student update, expected #httpCode"(Long id, Student student){
         when:
-            def response
-            def actualErrorMessage
-            try {
-                response = restTemplate.exchange(
-                        STUDENT_URL
-                        , HttpMethod.GET
-                        , new HttpEntity<>(headers)
-                        , List<Student>)
-            } catch (Exception e) {
-                actualErrorMessage = e.getMessage()
-            }
+        def response
+        def actualErrorMessage
+        try {
+            response = restTemplate.exchange(
+                    STUDENT_URL + id
+                    , HttpMethod.PUT
+                    , new HttpEntity<>(student, headers)
+                    , Student)
+        } catch (Exception e) {
+            actualErrorMessage = e.getMessage()
+        }
         then:
+        def actualStudent = response.getBody()
+        actualStudent == updatedStudent
+        response.getStatusCode() == httpCode
+        where:
+        id    |   student                                                             | httpCode         || updatedStudent
+        6     |   new Student("Michael", "Scofield", LocalDate.of(1992, 3, 4), null)  | HttpStatus.OK    || new Student(6, "Michael", "Scofield", LocalDate.of(1992, 3, 4), LocalDateTime.of(2021, 12, 12, 1, 2, 3))
+    }
+
+    def "Student save, expected #httpCode"(Student student){
+        when:
+        def response
+        def actualErrorMessage
+        try{
+            response = restTemplate.exchange(
+                    STUDENT_URL,
+                    HttpMethod.POST,
+                    new HttpEntity<Student>(student, headers),
+                    Student)
+        } catch (Exception ex){
+            actualErrorMessage = ex.getMessage();
+        }
+        then:
+        def actualStudent = response.getBody()
+        actualStudent == savedStudent
+        response.getStatusCode() == httpCode
+
+        where:
+        student                                                                                              | httpCode        | savedStudent
+        new Student("Viktor", "Shilay", LocalDate.of(1999, 1, 22), LocalDateTime.of(2022, 8, 13, 9, 20))     | HttpStatus.OK   | new Student(7, "Viktor", "Shilay", LocalDate.of(1999, 1, 22), LocalDateTime.of(2022, 8, 13, 9, 20))
+    }
+
+    def "Students get all, expected 6"() {
+        when:
+        def response
+        def actualErrorMessage
+        try {
+            response = restTemplate.exchange(
+                    STUDENT_URL
+                    , HttpMethod.GET
+                    , new HttpEntity<>(headers)
+                    , List<Student>)
+        } catch (Exception e) {
+            actualErrorMessage = e.getMessage()
+        }
+        then:
+        List<Student> actualStudents = response.getBody()
+        actualStudents.size() == 6
+    }
+
+    def "Students get by #firstName, expected #count"() {
+        when:
+        def response
+        def actualErrorMessage
+        try {
+            response = restTemplate.exchange(
+                    STUDENT_URL + "search?first_name=" + firstName
+                    , HttpMethod.GET
+                    , new HttpEntity<>(headers)
+                    , List<Student>)
+        } catch (Exception e) {
+            actualErrorMessage = e.getMessage()
+        }
+        then:
+        List<Student> actualStudent = response.getBody()
+        actualStudent.size() == count
+        where:
+        firstName       || count
+        'Aliko'         || 1
+        'Bill'          || 2
+        'Folrunsho'     || 3
+        'Dima'          || 0
+    }
+
+    def "Students get by #lastName, expected #count"() {
+        when:
+        def response
+        def actualErrorMessage
+        try {
+            response = restTemplate.exchange(
+                    STUDENT_URL + "search?last_name=" + lastName
+                    , HttpMethod.GET
+                    , new HttpEntity<>(headers)
+                    , List<Student>)
+        } catch (Exception e) {
+            actualErrorMessage = e.getMessage()
+        }
+        then:
+        List<Student> actualStudent = response.getBody()
+        actualStudent.size() == count
+        where:
+        lastName       || count
+        'Dangote'      || 1
+        'Gates'        || 1
+        'Kek'          || 0
+    }
+
+    def "Delete student by #id, expected #code"(Long id) {
+        when:
+        def response
+        def actualErrorMessage
+        try {
+            response = restTemplate.exchange(
+                    STUDENT_URL + id
+                    , HttpMethod.DELETE
+                    , new HttpEntity<>(headers)
+                    , void)
+        } catch (Exception e) {
+            actualErrorMessage = e.getMessage()
+        }
+        then:
+        response.getStatusCode() == code
+        where:
+        id || code
+        1  || HttpStatus.NO_CONTENT
+        2  || HttpStatus.NO_CONTENT
+        3  || HttpStatus.NO_CONTENT
+        8  || HttpStatus.NOT_FOUND
+    }
+
+    def "Students get by date between #from and #to, expected #count"() {
+        when:
+        def response
+        def actualErrorMessage
+        try {
+            response = restTemplate.exchange(
+                    STUDENT_URL + "/search/birthdays?from=" + from + "&to=" + to
+                    , HttpMethod.GET
+                    , new HttpEntity<>(headers)
+                    , List<Student>)
+        } catch (RestClientException e) {
+            actualErrorMessage = e.getMessage()
+        }
+        then:
+        if (response != null) {
             List<Student> actualStudents = response.getBody()
-            assert actualStudents.size() == 6
-    }
-
-    @Unroll
-    def void "Students get by #name, expected #count"() {
-        when:
-            def response
-            def actualErrorMessage
-            try {
-                response = restTemplate.exchange(
-                        STUDENT_URL + "search?name=" + name
-                        , HttpMethod.GET
-                        , new HttpEntity<>(headers)
-                        , List<Student>)
-            } catch (Exception e) {
-                actualErrorMessage = e.getMessage()
-            }
-        then:
-            List<Student> actualStudent = response.getBody()
-            assert actualStudent.size() == count
-        where:
-            name        || count
-            'Aliko'     || 1
-            'Bill'      || 2
-            'Folrunsho' || 3
-            'Dima'      || 0
-    }
-
-    @Unroll
-    def void "Delete student by #id, expected #code"(Long id) {
-        when:
-            def response
-            def actualErrorMessage
-            try {
-                response = restTemplate.exchange(
-                        STUDENT_URL + id
-                        , HttpMethod.DELETE
-                        , new HttpEntity<>(headers)
-                        , Void.class)
-            } catch (Exception e) {
-                actualErrorMessage = e.getMessage()
-            }
-        then:
-            assert response.getStatusCode() == code
-        where:
-            id || code
-            1  || HttpStatus.NO_CONTENT
-            2  || HttpStatus.NO_CONTENT
-            3  || HttpStatus.NO_CONTENT
-            8  || HttpStatus.NOT_FOUND
-    }
-
-    @Unroll
-    def void "Students get by date between #from and #to, expected #count"() {
-        when:
-            def response
-            def actualErrorMessage
-            try {
-                response = restTemplate.exchange(
-                        STUDENT_URL + "/search/birthdays?from=" + from + "&to=" + to
-                        , HttpMethod.GET
-                        , new HttpEntity<>(headers)
-                        , List<Student>)
-            } catch (HttpClientErrorException e) {
-                response = e.getStatusCode()
-                actualErrorMessage = e.getMessage()
-            }
-        then:
-            if (response != null) {
-                List<Student> actualStudents = response.getBody()
-                assert actualStudents.size() == count
-                assert response.getStatusCode() == status
-            }
+            actualStudents.size() == count
+            response.getStatusCode() == status
+        } else {
+            count == null
+            status == HttpStatus.BAD_REQUEST
+        }
 
         where:
-            from         | to           | count || status
-            '1997-03-17' | '1997-12-31' | 2     || HttpStatus.OK
-            '1955-12-31' | '1997-12-31' | 6     || HttpStatus.OK
-            '1993-06-03' | '1993-06-03' | 1     || HttpStatus.OK
-            '2000-06-03' | '2000-12-03' | 0     || HttpStatus.OK
-            //""           | ""           | null  || HttpStatus.BAD_REQUEST
+        from         | to           | count || status
+        '1997-03-17' | '1997-12-31' | 2     || HttpStatus.OK
+        '1955-12-31' | '1997-12-31' | 6     || HttpStatus.OK
+        '1993-06-03' | '1993-06-03' | 1     || HttpStatus.OK
+        '2000-06-03' | '2000-12-03' | 0     || HttpStatus.OK
+        ' '          | ' '          | null  || HttpStatus.BAD_REQUEST
     }
 }
